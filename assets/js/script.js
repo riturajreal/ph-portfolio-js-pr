96b6c9431b53a3763be08021fb1bf63e3ccd784d
Rituraj Jha <riturajreal01@gmail.com>
2023-12-17 14:30:16 +0530

    aaded js code for responsiveness navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,22 @@ window.addEventListener("load", function() {
     loadingElement.classList.add("loaded");
     this.document.body.classList.remove("active");
 });
+
+// MOBILE NAV TOGGLER
+
+const [ navTogglers, navLinks, navbar, overlay] = [
+    document.querySelectorAll("[data-nav-toggler]"),
+    document.querySelectorAll("[data-nav-link"),
+    document.querySelector("[data-navbar"),
+    document.querySelector("[data-overlay"),
+];
+
+const toggleNav = function () {
+
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+}
+
+
+addEventOnElements(navTogglers, "click", toggleNav);
